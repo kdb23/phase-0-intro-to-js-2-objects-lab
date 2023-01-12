d413ff8a3caa308b435c1efea75d519fda4ce647
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-01-11 21:32:37 -0500

    commit tree b2ced608849da0b5ab3a758bcc61ed9696a4dfc3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name:"Max",
+    address: "123 Maple Ln",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newObj={...employee};
+    newObj [key]= value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const newObj={...employee};
+    newObj[key]= value;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
